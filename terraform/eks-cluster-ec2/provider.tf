d2aad4e5b260d43eefd2801c4325176898915aaa
“stella <[mbabidstella@yahoo.com]>
2023-11-15 22:48:04 -0500

    updated bucket and dynamodb table

--- terraform/eks-cluster-ec2/provider.tf
+++ terraform/eks-cluster-ec2/provider.tf
@@ -3,12 +3,12 @@
 terraform {
   backend "s3" {
     # Replace this with your bucket name!
-    bucket         = "k8s-project-bucket-2023"
+    bucket         = "velerobuck2023"
     key            = "jjtech/terraform.tfstate"
     region         = "us-east-1"
 
     # Replace this with your DynamoDB table name!
-    dynamodb_table = "jjtech-dynamodb"
+    dynamodb_table = "kdb2023"
   }
   
   required_providers {
